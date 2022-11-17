1f2bc8722128e8462280db4c323ac0153b94b0b8
AnesVrce <anes.vrce@chili-chips.xyz>
2022-11-17 01:48:49 +0100

    test

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -1,6 +1,12 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpressionEvaluatorTest {
+  @Test
+    void testiranje()
+  {}
+
 }
